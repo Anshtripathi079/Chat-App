87315118a7acd488d396fe4362dbf7c10507ed26
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-06-18 19:33:14 +0530

    added messages component and responsive styling

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>HEllo</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
---
+++ src/components/Message.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="" />
+      </div>
+    </div>
+  );
+};
+
+export default Message;
