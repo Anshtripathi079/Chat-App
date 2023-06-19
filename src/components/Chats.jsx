3bfc43ab15aae75b17339130c5c83a674eeca0f1
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-06-19 18:21:50 +0530

    fetching chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
   return (
     <div className="chats">
-      <div className="userChat">
-        <img src="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>HEllo</p>
-        </div>
-      </div>
+      {Object.entries(chats)?.map((chat) => {
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
+        </div>;
+      })}
     </div>
   );
 };
